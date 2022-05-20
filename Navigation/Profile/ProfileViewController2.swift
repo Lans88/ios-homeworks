0b17e5632c6463b764da899f3afe6ac13fef1c84
Artem <art001.163@gmail.com>
2022-05-20 07:36:29 +0400

    добавлена новая группа файлов проекта

---
+++ Navigation/Profile/ProfileViewController2.swift
@@ -0,0 +1,29 @@
+//
+//  ProfileViewController.swift
+//  Navigation
+//
+//  Created by Артем Бажанов on 18.05.2022.
+//
+
+import UIKit
+
+class ProfileViewController2: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = UIColor.lightGray
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

708c50ce7457cee308b90960970d3ae194dfc364
Artem <art001.163@gmail.com>
2022-06-27 08:56:17 +0400

    добавлены все элементы на экран согласно макета

--- Navigation/Profile/LogInViewController.swift
+++ Navigation/Profile/LogInViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class LogInViewController: UIViewController {
+class LogInViewController: UIViewController, UITextFieldDelegate {
     private var isNavigateionBarHidden: Bool = true
     
     private var logoImage: UIImageView = {
@@ -15,83 +15,107 @@ class LogInViewController: UIViewController {
         imageView.translatesAutoresizingMaskIntoConstraints = false
         return imageView
         }()
-    private lazy var logInTableView: UITableView = {
-        let tableView = UITableView()
-        tableView.translatesAutoresizingMaskIntoConstraints = false
-        tableView.layer.borderColor = UIColor.lightGray.cgColor
-        tableView.layer.borderWidth = 0.5
-        tableView.layer.cornerRadius = 10
-        tableView.backgroundColor = .systemGray6
-        tableView.dataSource = self
-        tableView.delegate = self
-        return tableView
-    }()
-    private var logInButton: UIButton {
+    private var logInButton: UIButton = {
         let button = UIButton()
-        button.translatesAutoresizingMaskIntoConstraints = false
+        button.clipsToBounds = true
         button.layer.cornerRadius = 10
-        button.layer.backgroundColor = UIColor.blue.cgColor
         button.setTitle("Log In", for: .normal)
         button.setTitleColor(.white, for: .normal)
-        //button.backgroundImage(for: .normal) = UIImage(contentsOfFile: "colorSet")
-        
-        
-        
+        button.backgroundColor = .systemBlue
+        button.setBackgroundImage(UIImage(named: "blue_pixel"), for: .normal)
+        button.addTarget(self, action: #selector(authorization), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
         return button
-    }
+    }()
+    private let scrollView: UIScrollView = {
+        let scroll = UIScrollView()
+        scroll.translatesAutoresizingMaskIntoConstraints = false
+        return scroll
+    }()
+    lazy var logInText: UITextField = {
+        let logtext = UITextField()
+        logtext.translatesAutoresizingMaskIntoConstraints = false
+        logtext.placeholder = "Email or phone"
+        logtext.textAlignment = .left
+        logtext.textColor = .black
+        logtext.backgroundColor = .systemGray6
+        logtext.font = .systemFont(ofSize: 16)
+        logtext.layer.borderColor = UIColor.lightGray.cgColor
+        logtext.layer.cornerRadius = 10
+        logtext.layer.borderWidth = 0.5
+        logtext.clearButtonMode = .whileEditing
+        logtext.autocapitalizationType = .none
+        logtext.delegate = self
+        return logtext
+    }()
+    lazy var passText: UITextField = {
+        let pass = UITextField()
+        pass.translatesAutoresizingMaskIntoConstraints = false
+        pass.placeholder = "Password"
+        pass.textAlignment = .left
+        pass.textColor = .black
+        pass.backgroundColor = .systemGray6
+        pass.font = .systemFont(ofSize: 16)
+        pass.layer.borderColor = UIColor.lightGray.cgColor
+        pass.layer.cornerRadius = 10
+        pass.layer.borderWidth = 0.5
+        pass.clearButtonMode = .whileEditing
+        pass.autocapitalizationType = .none
+        pass.isSecureTextEntry = true
+        pass.delegate = self
+        return pass
+    }()
+    let notification = NotificationCenter.default
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.isNavigationBarHidden = true
-        layoutTable()
-        layoutLogo()
-        layoutLogInButton()
         view.backgroundColor = .white
+        layout()
     }
-    private func layoutLogo() {
+    @objc func authorization() {
+        let profileViewController = ProfileViewController()
+        navigationController?.pushViewController(profileViewController, animated: true)
+    }
+    private func layout() {
         view.addSubview(logoImage)
+        view.addSubview(logInText)
+        view.addSubview(logInButton)
+        view.addSubview(passText)
+        view.addSubview(scrollView)
         NSLayoutConstraint.activate([
             logoImage.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             logoImage.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 120),
             logoImage.widthAnchor.constraint(equalToConstant: 100),
-            logoImage.heightAnchor.constraint(equalToConstant: 100)
-        ])
-    }
-    func layoutTable() {
-        view.addSubview(logInTableView)
-        
-        NSLayoutConstraint.activate([
-            logInTableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 340),
-            logInTableView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            logInTableView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
-            logInTableView.heightAnchor.constraint(equalToConstant: 100)
-        ])
-    }
-    func layoutLogInButton() {
-        view.addSubview(logInButton)
-        NSLayoutConstraint.activate([
+            logoImage.heightAnchor.constraint(equalToConstant: 100),
+            
+            logInText.topAnchor.constraint(equalTo: logoImage.bottomAnchor, constant: 120),
+            logInText.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            logInText.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            logInText.heightAnchor.constraint(equalToConstant: 50),
+            
+            passText.topAnchor.constraint(equalTo: logInText.bottomAnchor),
+            passText.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            passText.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            passText.heightAnchor.constraint(equalToConstant: 50),
+            
+            logInButton.topAnchor.constraint(equalTo: passText.bottomAnchor, constant: 16),
             logInButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
-            logInButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16)
+            logInButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            logInButton.heightAnchor.constraint(equalToConstant: 50),
+            
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
         ])
     }
-  
-   
-
 
 
 }
 
-extension LogInViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 2
-    }
+
     
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        UITableViewCell()
-    }
     
    
-}
-extension LogInViewController: UITableViewDelegate {
-    
-}
+

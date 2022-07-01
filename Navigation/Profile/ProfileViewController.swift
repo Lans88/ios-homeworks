35af153b40017e5fc1693e4d0832bc6f63e82563
Artem <art001.163@gmail.com>
2022-07-01 08:28:11 +0400

    создание таблицы, структуры поста, добавление контента

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -8,42 +8,50 @@
 import UIKit
 
 class ProfileViewController: UIViewController {
-    
-    var profileHeaderView = ProfileHeaderView()
-    
-    var newButton: UIButton = {
-        let nButton = UIButton()
-        nButton.backgroundColor = .green
-        nButton.setTitle("New button", for: .normal)
-        nButton.layer.cornerRadius = 4.0
-        nButton.translatesAutoresizingMaskIntoConstraints = false
-        return nButton
+    private lazy var publicTable: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.dataSource = self
+        tableView.delegate = self
+        return tableView
     }()
+   
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        title = "Профиль"
-        view.backgroundColor = .lightGray
-        view.addSubview(profileHeaderView)
-        view.addSubview(newButton)
-        
-        NSLayoutConstraint.activate([
-            newButton.leadingAnchor.constraint(equalTo: view.leadingAnchor),
-            newButton.trailingAnchor.constraint(equalTo: view.trailingAnchor),
-            newButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            newButton.heightAnchor.constraint(equalToConstant: 50),
-        ])
+        layout()
        
     }
-    
-    override func viewWillLayoutSubviews() {
-        super.viewWillLayoutSubviews()
-        profileHeaderView.frame = CGRect(x: 0, y: view.safeAreaInsets.top, width: view.bounds.width, height: view.bounds.height - view.safeAreaInsets.top - view.safeAreaInsets.bottom)
-
+    private func layout() {
+        view.addSubview(publicTable)
+        NSLayoutConstraint.activate([
+            publicTable.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            publicTable.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            publicTable.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            publicTable.trailingAnchor.constraint(equalTo: view.trailingAnchor)
+        ])
     }
-
-    
-
 }
 
-
+extension ProfileViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        publicArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: nil)
+        var content: UIListContentConfiguration = cell.defaultContentConfiguration()
+        content.text = publicArray[indexPath.row].author
+        content.image = UIImage(named: publicArray[indexPath.row].image)
+        content.text = publicArray[indexPath.row].description
+        content.text = "Likes: \(publicArray[indexPath.row].likes)"
+        content.text = "Views: \(publicArray[indexPath.row].views)"
+        cell.contentConfiguration = content
+        return cell
+    }
+    
+    
+}
+extension ProfileViewController: UITableViewDelegate {
+    
+}
